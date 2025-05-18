adf59e27a1050bdce2b0a6d9ba0175d485dd0d68
princemahmood117 <princemahmood117@gmail.com>
2025-05-18 18:03:03 +0600

    implemented logic for X and O

--- src/App.jsx
+++ src/App.jsx
@@ -13,42 +13,57 @@ Game
 
 import { useState } from "react";
 
-function Square() {
+function Square({value,onSquareClick}) {
 
-  const [value,setValue] = useState(null)
-
-  function handleClick () {
-    setValue('X')
-  }
   return (
   <>
-    <button onClick={handleClick} className="m-1 bg-white border border-gray-300 h-12 w-12 leading-9">{value}</button>
+    <button onClick={onSquareClick} className="m-1 bg-white border border-gray-300 h-12 w-12 leading-9">{value}</button>
   </>
      )
 }
  
 
 function Board() {
+
+  const [squares,setSquares] = useState(Array(9).fill(null))
+
+  const [isXNext,setIsXNext] = useState(true)
+
+
+  function handleClick(i) {
+
+    const nextSquares = squares.slice()
+
+    if(isXNext) {
+      nextSquares[i] = 'X'
+    }
+    else {
+      nextSquares[i] = 'O'
+    }
+    setSquares(nextSquares)
+    setIsXNext(!isXNext)
+
+  }
   
   return (
     <div className="text-center">
       <h1 className="text-3xl my-6">Tic Tac Toe Game</h1>
-    <div>
-       <Square></Square>
-       <Square></Square>
-       <Square></Square>          
+    <div className="flex justify-center">
+       <Square value={squares[0]} onSquareClick={()=> handleClick(0)}></Square>
+       <Square value={squares[1]} onSquareClick={()=> handleClick(1)}></Square>
+       <Square value={squares[2]} onSquareClick={()=> handleClick(2)}></Square>          
     </div>
 
-    <div>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>   
+    <div className="flex justify-center">
+      <Square value={squares[3]} onSquareClick={()=> handleClick(3)}></Square>
+      <Square value={squares[4]} onSquareClick={()=> handleClick(4)}></Square>
+      <Square value={squares[5]} onSquareClick={()=> handleClick(5)}></Square>   
     </div>
 
-    <div>
-      <Square></Square>
-      <Square></Square>
-      <Square></Square>
+    <div className="flex justify-center">
+      <Square value={squares[6]} onSquareClick={()=> handleClick(6)}></Square>
+      <Square value={squares[7]} onSquareClick={()=> handleClick(7)}></Square>
+      <Square value={squares[8]} onSquareClick={()=> handleClick(8)}></Square>
     </div>
     </div>
   )
