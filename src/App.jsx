3eeca8456211a27520ce7a81bd34c90c70f8c3af
princemahmood117 <princemahmood117@gmail.com>
2025-05-18 19:06:46 +0600

    tic-tac-toe functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function Square({value,onSquareClick}) {
 
   return (
   <>
-    <button onClick={onSquareClick} className="m-1 bg-white border border-gray-300 h-12 w-12 leading-9">{value}</button>
+    <button onClick={onSquareClick} className="m-1 bg-white border border-gray-300 h-12 w-12 leading-9 cursor-pointer">{value}</button>
   </>
      )
 }
@@ -25,14 +25,29 @@ function Square({value,onSquareClick}) {
 
 function Board() {
 
-  const [squares,setSquares] = useState(Array(9).fill(null))
+  const [squares,setSquares] = useState(Array(9).fill(null))  // ৯ টা অ্যারে সেখানের সবগুলায় ডিফল্ট মান 'Null' যা শর্তের ভিত্তিতে চেঞ্জ হয়ে X/O শো করবে
 
   const [isXNext,setIsXNext] = useState(true)
 
+  const winner = calculateWinner(squares);
+  let status;
 
+  if(winner) {
+    status = `Winner is : ${winner}`
+  }
+  else {
+    status = "Next Player is : " + (isXNext ? 'X' : 'O') 
+  }
+
+  
+  // squares এ ক্লিক এর ফাংশনালিটি এখান থেকে কন্ট্রল করা হবে
   function handleClick(i) {
 
-    const nextSquares = squares.slice()
+    const nextSquares = squares.slice()  // Duplicate array creation so main array(squares) will be unchanged
+
+    if(squares[i] || calculateWinner(squares)) {   // স্কয়ারের ভেতর কোনো ভ্যালু থাকলে null রিটার্ন করবে(cant be chaged the value),না থাকলে নিচের X/O বসানোর ফাংশনালিটি এপ্লাই করবে
+      return;
+    }
 
     if(isXNext) {
       nextSquares[i] = 'X'
@@ -44,10 +59,37 @@ function Board() {
     setIsXNext(!isXNext)
 
   }
+
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6],
+    ]
+
+    for(let i = 0; i < lines.length; i++) {
+      const [a,b,c] = lines[i]
+
+      if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+      {
+        return squares[a]  // winner is found
+      }
+    }
+    return null; // returns null if the winner is not found
+  }
   
   return (
-    <div className="text-center">
+    <div className="text-center cursor-pointer">
       <h1 className="text-3xl my-6">Tic Tac Toe Game</h1>
+
+      <p>Status : {status}</p>
+      
     <div className="flex justify-center">
        <Square value={squares[0]} onSquareClick={()=> handleClick(0)}></Square>
        <Square value={squares[1]} onSquareClick={()=> handleClick(1)}></Square>
